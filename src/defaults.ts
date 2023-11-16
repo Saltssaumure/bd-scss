c5cf9e79fbbf3c7466302883a3d390114531a02d
Saltssaumure <themes@saltssaumure.net>
2023-11-16 18:08:53 +0000

    Run prettier

--- src/defaults.ts
+++ src/defaults.ts
@@ -3,21 +3,22 @@ import { getConfig, getDataFolder } from "./utils.js";
 const config = await getConfig();
 
 export const DEFAULTS = (clientMod: string) => {
-    return ({
-    meta: {
-        name: `${config?.meta.name} - Test`,
-        author: `Saltssaumure`,
-        authorLink: `https://github.com/Saltssaumure`,
-        version: `${config?.meta.version}`,
-        source: `https://github.com/Saltssaumure/${config?.meta.repo}`,
-        description: `Dev version of https://github.com/Saltssaumure/${config?.meta.repo}.`
-    },
-    dev: {
-        target: `scss/${config?.meta.scss}.scss`,
-        output: getDataFolder(clientMod)
-    },
-    build: {
-        target: `scss/${config?.meta.scss}.scss`,
-        output: ""
-    } });
+    return {
+        meta: {
+            name: `${config?.meta.name} - Test`,
+            author: `Saltssaumure`,
+            authorLink: `https://github.com/Saltssaumure`,
+            version: `${config?.meta.version}`,
+            source: `https://github.com/Saltssaumure/${config?.meta.repo}`,
+            description: `Dev version of https://github.com/Saltssaumure/${config?.meta.repo}.`
+        },
+        dev: {
+            target: `scss/${config?.meta.scss}.scss`,
+            output: getDataFolder(clientMod)
+        },
+        build: {
+            target: `scss/${config?.meta.scss}.scss`,
+            output: ""
+        }
+    };
 };
